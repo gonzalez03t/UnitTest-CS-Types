e8fe6bbeaff1da006e0b496f8572bcc585eac9f3
Jesus Gonzalez <gonzalez0614@ufl.edu>
2019-05-23 11:23:57 -0400

    Test Reference Types

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,14 +1,51 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest_CS_Types
 {
+    public class Invoice
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+
     [TestClass]
-    public class UnitTest1
+    public class ReferenceTypesAndValueTypes
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IdentityTest()
         {
+            Invoice firstInvoice = new Invoice(); //Instantiate first invoice object
+            firstInvoice.ID = 1;
+            firstInvoice.Description = "Test";
+            firstInvoice.Amount = 0.0M;
+
+            Invoice secondInvoice = new Invoice(); //Instantiate first invoice object
+            secondInvoice.ID = 1;
+            secondInvoice.Description = "Test";
+            secondInvoice.Amount = 0.0M;
+
+            //Testing Reference Types
+            Assert.IsFalse(object.ReferenceEquals(secondInvoice, firstInvoice));
+            Assert.IsTrue(firstInvoice.ID == 1);
+
+            secondInvoice.ID = 2;
+
+            Assert.IsTrue(secondInvoice.ID == 2);
+            Assert.IsTrue(firstInvoice.ID == 1);
+
+            secondInvoice = firstInvoice;
+
+            Assert.IsTrue(object.ReferenceEquals(secondInvoice, firstInvoice));
+
+            secondInvoice.ID = 5;
+
+            Assert.IsTrue(firstInvoice.ID == 5);
         }
+
     }
 }
